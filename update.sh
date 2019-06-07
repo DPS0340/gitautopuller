8a364a48306f63dfac6021570fbdce3ca6e7b79d
jiho lee <jiholee@kpu.ac.kr>
2019-06-07 14:26:36 +0900

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "path = $1"
+echo "script = $2"
+
+cd "$1"
+process=$(ps -ef | grep "$2" | awk '{print $2}')
+
+if [ ! -z "$str" ]; then
+        pkill -9 $process
+fi
+
+echo $(git pull)
+nohup $2 &
